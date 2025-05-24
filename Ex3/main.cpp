d729591cf5eec63475488ced57f613ee2e09b9f0
Ofek Bar shalom <155369472+ofekbarshalom@users.noreply.github.com>
2025-05-24 19:31:27 +0300

    Add files via upload

---
+++ Ex3/main.cpp
@@ -0,0 +1,15 @@
+//ofekbarshalom@gmail.com
+
+#include "GUI/Screens.hpp"
+#include "src/Game.hpp"
+
+using namespace coup;  
+
+int main() {
+    if(launchOpeningScreen()) {
+        Game game_1{};
+        launchPlayerSetupScreen(game_1);
+    }
+    
+    return 0;
+}
---
+++ Ex3/src/Baron.hpp
@@ -0,0 +1,20 @@
+//ofekbarshalom@gmail.com
+
+#ifndef BARON_HPP
+#define BARON_HPP
+
+#include "Player.hpp"
+
+namespace coup {
+
+class Baron : public Player {
+    public:
+        Baron(Game& game, const string& name);
+
+        string getRole() const override;
+        void invest();  // Pay 3 coins, receive 6 coins
+};
+
+}
+
+#endif // BARON_HPP
